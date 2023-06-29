a5d48b56546a54fbeca038b48baee669e1112e5d
Rufi <Rafajos9@gmail.com>
2023-06-29 16:47:59 -0400

    Added missing elements and carousel

--- js/script.js
+++ js/script.js
@@ -6,3 +6,60 @@ window.addEventListener('scroll',(e)=>{
 
     document.querySelector('.header').classList.remove('header-bg')
 })
+let customers
+
+window.addEventListener('load',()=>{
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:60,
+    responsiveClass:true,
+    center:true,
+    autoWidth:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            loop:true
+
+        },
+        600:{
+            items:3,
+            nav:false,
+            loop:true
+
+        },
+        1000:{
+            items:3,
+            nav:false,
+        }
+    }
+})
+
+customers = $('.carousel-customer')
+  customers.owlCarousel({
+    loop:true,
+    margin:20,
+    responsiveClass:true,
+    autoWidth:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+
+        }
+    }
+})
+
+
+
+})
+
+  // Go to the next item
+const leftPage = ()=>{
+  customers.trigger('prev.owl.carousel');
+}
+
+const rightPage = () =>{
+    customers.trigger('next.owl.carousel');
+
+}
