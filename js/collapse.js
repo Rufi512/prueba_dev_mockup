4c8d7c51e10e0545bf05a7168b6f5f8694a3c09d
Rufi <Rafajos9@gmail.com>
2023-06-30 10:54:36 -0400

    Updates + + Added page shop + Added some images for the pages + Fixs styles

--- js/collapse.js
+++ js/collapse.js
@@ -1,15 +1,12 @@
 const collapse = (id, arrow) => {
 	const coll = document.getElementById(id);
 	const arrowIcon = document.getElementById(arrow);
-	console.log("click");
 
 	if (coll.dataset.active === "inactive") {
 		coll.style.maxHeight = "1000px";
 		coll.dataset.active = "active";
 		arrowIcon.style.transform = "rotate(210deg)";
 		arrowIcon.style.transformOrigin = "6px 11px";
-
-		console.log("pass");
 		return;
 	}
 
