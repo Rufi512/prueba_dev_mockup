18dc8d9d9e20e56e95344bb33f090dc944dbef64
rufi512 <rafajos9@gmail.com>
2023-07-02 18:12:47 -0400

    Add changes +Added video to customers reviews +Added top header in mobile +Added changes in Home Page +Fixs

--- js/carousel-customer.js
+++ js/carousel-customer.js
@@ -24,3 +24,29 @@ customers.owlCarousel({
       customers.trigger('next.owl.carousel');
   
   }
+
+  //Team videos
+const playVideo = (id)=>{
+    const videoContainer = document.getElementById(id)
+    videoContainer.querySelector('.player-preview').style.opacity = 0 
+    videoContainer.querySelector('.player-preview').style.visibility = 'hidden' 
+    videoContainer.querySelector('.video-player').play()
+  }
+  
+  const stopVideos = ()=>{
+    const containersVideos = document.querySelectorAll('.video-player')
+    containersVideos.forEach((el)=>{
+      el.pause()
+      el.currentTime = 0
+    })
+  
+    document.querySelectorAll('.player-preview').forEach((el)=>{
+      el.style.opacity = 1
+      el.style.visibility = 'visible'
+    })
+  }
+  
+
+  customers.on('changed.owl.carousel', function(event) {
+    stopVideos()
+})
